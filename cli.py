c66ddd0ae22feb44ea76d10b65da2886b2d71f4d
Dylan Baker <dylang.baker@googlemail.com>
2019-05-17 08:04:48 +0100

    testing custom function in jinja

--- cli.py
+++ cli.py
@@ -8,9 +8,9 @@ Hello,
 ## This is an email from Dylan. 
 
 {% if new_clients_count < 0 %}
-We lost {{ new_clients_count|abs }} clients yesterday.
+We lost {{ multiply_by_two(new_clients_count)|abs }} clients yesterday.
 {% else %}
-We added {{ new_clients_count }} new clients yesterday.
+We added {{ multiply_by_two(new_clients_count) }} new clients yesterday.
 {% endif %}
 """
 
@@ -18,11 +18,22 @@ We added {{ new_clients_count }} new clients yesterday.
 def cli():
 	pass
 
+def multiply_by_two(number):
+	return number*2
+
 @click.command()
 @click.argument('new_clients_count')
 def run(new_clients_count):
+
+	# Add the necessary functions to the Jinja environment
+	template = Template(TEMPLATE)
+	template.globals['multiply_by_two'] = multiply_by_two
+
+	# Render the Jinja2 template, inserting the relevant values
 	extensions = ['extra', 'smarty']
 	doc = Template(TEMPLATE).render(new_clients_count=int(new_clients_count))
+
+	# Convert the Markdown as HTML5
 	html = markdown.markdown(doc, extensions=extensions, output_format='html5')
 	print(html)
 	
